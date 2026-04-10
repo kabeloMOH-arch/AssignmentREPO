7eecb5c4c231ff3b7c9de6a54c88d5ebad623dea
Admin <Admin@LAPTOP-398AQ1IG.lan>
2026-04-10 20:38:57 +0200

    added String Username added String Password added String CellNumber

---
+++ NetBeansProjects/Assignment/src/assignment/Assignment.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package assignment;
+
+class Part1login{
+ String  Username;
+ String  Password;
+ String  cellPhoneNumber;
+ 
+ 
+}
+public class Assignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
