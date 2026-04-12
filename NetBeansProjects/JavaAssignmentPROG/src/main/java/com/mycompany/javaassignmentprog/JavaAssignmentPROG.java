9e823c51186dbe23daeba820ba1ed28da48c7efc
Admin <Admin@LAPTOP-398AQ1IG.lan>
2026-04-12 12:26:02 +0200

    this is something ig

---
+++ NetBeansProjects/JavaAssignmentPROG/src/main/java/com/mycompany/javaassignmentprog/JavaAssignmentPROG.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaassignmentprog;
+
+/**
+ *
+ * @author Admin
+ */
+public class JavaAssignmentPROG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
