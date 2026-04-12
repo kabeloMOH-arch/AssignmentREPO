ab465339e1a981ceec59fb501a3d5f9a1edb19c5
Admin <Admin@LAPTOP-398AQ1IG.lan>
2026-04-12 20:22:51 +0200

    Initialized String Username String Password String cellNumber
    checked if the username holds less than 5 characters
    Boolean checkUsername()

--- NetBeansProjects/Assignment7/src/assignment7/Assignment7.java
+++ NetBeansProjects/Assignment7/src/assignment7/Assignment7.java
@@ -4,17 +4,44 @@
  */
 package assignment7;
 
+class login{
+    String Username;
+    String Password;
+    String CellNumber;
+
+ boolean checkUsername(String Username){
+     if(Username.length()==5 && Username.contains("_")){
+      return true;   
+     }else{
+         System.out.println("Username is not properly Formatted,please ensure that your username contains an underscore and is no more than 5 characters");
+     }return false;
+  }
+ 
+ boolean checkPasswordComplexity(String Password){
+     if(Password.length()==8 && Password.contains("#D//")){
+     return true;
+     }else{
+         System.out.println("Password is not properly Formatted,please ensure that your username contains an underscore and is no more than 5 characters");
+     }return false;
+     
+ 
+ }
+ boolean checkCellPhoneNumber(String cellPhoneNumber){
+     if(cellPhoneNumber.startsWith("+") && cellPhoneNumber.length()<=10 ){
+      return true;    
+     }else{ System.out.println("Cellphone number does not contain international code or is not formatted correctly");
+     return false;
+ }
+
+ }
+
+ 
+}     
+
 /**
  *
  * @author Admin
  */
-public class Assignment7 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
-}
+
---
+++ NetBeansProjects/Assignment7/src/assignment7/quick_chat.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package assignment7;
+import java.util.Scanner;
+/**
+ *
+ * @author Admin
+ */
+public class quick_chat {
+
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+      
+        boolean registered = false;
+
+   
+        System.out.println("============================================");
+        System.out.println("         Welcome to QuickChat");
+        System.out.println("============================================");
+        System.out.println("\n--- Create an Account ---\n");
+
+        System.out.print("Enter first name: ");
+        String firstName = scanner.nextLine().trim();
+
+        System.out.print("Enter last name: ");
+        String lastName = scanner.nextLine().trim();
+
+       
+        System.out.println("Enter your UserName Below");
+        String Username = scanner.nextLine();
+        
+        System.out.println("Enter your Password");
+        String Password = scanner.nextLine();
+        
+            }
+    }
+    
+
